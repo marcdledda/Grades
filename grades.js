17758b8e284ce623f9f74c686286ba58294be5b6
marcdledda <marcdl636@gmail.com>
2018-01-30 15:43:41 -0600

    grades legends

--- grades.js
+++ grades.js
@@ -1,1 +1,22 @@
 console.log("JS TEST");
+
+// GRADES
+// 50-60 = F
+// 61-70 = D
+// 71-80 = C
+// 81-90 = B
+// 91-100 = A
+
+
+// ---EXERCISE---
+const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+const grades = {} // You'll need to change this line of code
+
+for (let i = 0; i < scores.length; i++) {
+    /*
+      If the score is greater than 90, increment grades.A by 1.
+
+      You can use a series of `if/then` blocks, but you could
+      also achieve this with a switch statement.
+    */
+}
